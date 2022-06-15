52615427cb80b4276facccfe45ed5518c6f66637
Knarkzel <knarkzel@gmail.com>
2022-06-15 20:39:14 +0200

    Camera movement working

--- src/main.zig
+++ src/main.zig
@@ -11,24 +11,33 @@ fn run(video: *ogc.Video) !void {
     // State
     var x: f32 = 0;
     var y: f32 = 0;
+    var cx: f32 = 0;
+    var cz: f32 = 0;
     var angle: f32 = 0;
     const speed: f32 = 0.1;
 
     while (true) {
         // Set camera
-        video.set_camera(.{ .perspective = .{ .target = .{ .x = x, .y = y, .z = -1 } } });
+        video.set_camera(.{
+            .perspective = .{
+                .target = .{ .x = x, .y = y, .z = -10 },
+                .position = .{ .x = cx, .y = 0, .z = cz },
+            },
+        });
 
         // Movement
         for (Pad.update()) |controller, i| {
             if (controller) {
                 x -= Pad.stick_x(i) * speed;
                 y -= Pad.stick_y(i) * speed;
+                cx -= Pad.sub_stick_x(i) * speed;
+                cz -= Pad.sub_stick_y(i) * speed;
                 if (Pad.button_down(.start, i)) std.os.exit(0);
             }
         }
 
         // Create cuboid with different colors
-        var cube = Cuboid.init(0, 0, -10, 2, 2, 2, 0xAABBCCFF);
+        var cube = Cuboid.init(0, 0, -10, 2, 2, 2);
         cube.set_colors(.{
             0x000000FF,
             0xFF0000FF,
