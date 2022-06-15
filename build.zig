06d1cc5a324c03c35b3ebe2ad629bef8983bf848
Knarkzel <knarkzel@gmail.com>
2022-06-15 19:17:36 +0200

    3d cube working

--- build.zig
+++ build.zig
@@ -2,6 +2,6 @@ const std = @import("std");
 const ogc = @import("vendor/ogc/build.zig");
 
 pub fn build(builder: *std.build.Builder) !void {
-    var obj = try ogc.target_wii(builder, .{ .name = "example" });
+    var obj = try ogc.target_wii(builder, .{ .name = "example", .wii_ip = "192.168.11.171" });
     obj.addPackagePath("ogc", "vendor/ogc/src/main.zig");
 }
--- src/main.zig
+++ src/main.zig
@@ -8,18 +8,24 @@ export fn main(_: c_int, _: [*]const [*:0]const u8) void {
 }
 
 fn run(video: *ogc.Video) !void {
+    var angle: f32 = 0;
     while (true) {
-        // Draw square
-        video.start();
-        var cube = Cuboid.init(2, 1, -10, 2, 2, 2, 0xAABBCCFF);
+        angle += 1;
+        var cube = Cuboid.init(-1, -1, -10, 2, 2, 2, 0xAABBCCFF);
         cube.set_colors(.{
             0x000000FF,
-            0xF00000FF,
             0xFF0000FF,
-            0xFFF000FF,
             0xFFFF00FF,
-            0xFFFFF0FF,
+            0xFFFFFFFF,
+            0x00FFFFFF,
+            0x0000FFFF,
         });
+        const center = cube.center();
+        cube.rotate_x(center, angle);
+        cube.rotate_z(center, angle / 8);
+
+        // Draw square
+        video.start();
         cube.draw();
         video.finish();
     }
